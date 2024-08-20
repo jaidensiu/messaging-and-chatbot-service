3ff5657a0365c8dece03bf8ddf58796e6ddee144
jaidensiu <jaidensiu@gmail.com>
2024-08-20 01:15:45 -0700

    chore: code formatting

--- server/internal/user/user_repository.go
+++ server/internal/user/user_repository.go
@@ -37,7 +37,7 @@ func (r *repository) CreateUser(ctx context.Context, user *User) (*User, error) 
 func (r *repository) GetUserByEmail(ctx context.Context, email string) (*User, error) {
 	u := User{}
 	query := "SELECT id, email, username, password FROM users WHERE email = $1"
-	
+
 	err := r.db.QueryRowContext(ctx, query, email).Scan(&u.ID, &u.Email, &u.Username, &u.Password)
 	if err != nil {
 		return &User{}, nil
